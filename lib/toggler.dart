a21ea2505e2b46c2d23ddc35c2ff8d207d51da04
ohir <ohir@fairbe.org>
2022-10-31 12:24:01 +0100

    detach -> detachSelf

--- lib/toggler.dart
+++ lib/toggler.dart
@@ -433,6 +433,9 @@ class Toggler {
       isDs ? ds = nEW : bits = nEW;
       return;
     }
+    // TODOlater CONSIDER allowing newS changes on the fix Go
+    // ie. if live on hold single changes should register straight
+    // to the newS.
     if (hh & (1 << _bf) != 0) return; // live but on hold
 
     final oldS = Toggler(bits: bits, ds: ds, rg: rg, hh: hh);
@@ -510,7 +513,9 @@ abstract class ToggledNotifier {
   /// implementations may inform about how many points observe
   int get observers => -1;
 
-  /// cleanup code should go to `detach`
-  void detach() {}
+  /// deregistering and cleanup code should go to `detachSelf`
+  /// It is expected that it is the Notifier having the most knowledge who
+  /// is listening.
+  void detachSelf() {}
 }
 // coverage:ignore-end
